880ba6caad7a893246e08d08c035a82a5fcabfd6
Brandon Wright <thewrightbrandon@gmail.com>
2021-05-28 18:55:30 -0400

    Added Comment Components

---
+++ src/components/AddComment.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+class AddComment extends React.Component {
+
+  state = {
+    mood: this.props.mood.id,
+    name: '',
+    body: '',
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.addComment(this.state)
+      this.setState({
+        name: '',
+        body: '',
+      })
+  }
+
+  render = () => {
+    return (
+      <div>
+        <details>
+          <summary>Add Comment</summary>
+            <form onSubmit={this.handleSubmit}>
+              <label htmlFor="name">Title</label>
+              <input type="text" id="name" onChange={this.handleChange} value={this.state.name} />
+              <br />
+              <label htmlFor="body">Comment</label>
+              <textarea type="text" id="body" onChange={this.handleChange} value={this.state.body}
+              placeholder="Comment here..." rows="6" cols="50">
+              </textarea>
+              <br />
+              <input type="submit" value="Add Comment" />
+            </form>
+        </details>
+      </div>
+    )
+  }
+}
+
+export default AddComment
